6ddcbb89f84ac428bc05242d2660515b706dec81
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2023-12-22 22:24:58 +0600

    get and post methode live example is here

---
+++ class4.1/iqus-post&get-method/get-method.php
@@ -0,0 +1,9 @@
+Your Email is: <?php echo $_GET['email'];?><br>
+Your password is: <?php echo $_GET['password'];?><br>
+<hr>
+<pre>
+    <?php var_dump($_REQUEST);?>
+</pre>
+<pre>
+    <?php echo $_SERVER['REQUEST_METHODE'];?>
+</pre>
---
+++ class4.1/iqus-post&get-method/post-method.php
@@ -0,0 +1,9 @@
+Your email is : <?php echo $_POST['email']?> <br>
+Your password is : <?php echo $_POST['password']?> <br>
+
+<!-- SIR'S -->
+
+<hr>
+<pre>
+    <?php var_dump($_REQUEST);?>
+</pre>
