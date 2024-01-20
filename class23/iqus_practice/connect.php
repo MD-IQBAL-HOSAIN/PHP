37a0fa97b11680a2df2eaf46296da1e2ce50d403
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-01-20 20:34:48 +0600

    make connection with database and get value

---
+++ class23/iqus_practice/connect.php
@@ -0,0 +1,7 @@
+<?php
+
+$conct = new mysqli('localhost','root','','sakila') or die('could not connect to databases'); //hostName + userName + password + database.
+$conct->set_charset("utf8");
+
+
+?>
---
+++ class23/iqus_practice/main_page.php
@@ -0,0 +1,21 @@
+<?php
+
+require "connect.php";
+
+$todonto = "select * from actor";
+$show = $conct->query($todonto);
+//echo $show;
+echo "<pre>";
+var_dump($show);
+echo "</pre>";
+echo "<hr>";
+
+if ($show->num_rows) {
+    while ($shows = $show->fetch_assoc()) {
+        echo "id-".$shows['actor_id'] . "<br><br>";
+        echo "First_Name:".$shows['first_name'] . "<br>";
+        echo "First_Name:".$shows['last_name'] . "<br>";
+    }
+}
+
+?>
