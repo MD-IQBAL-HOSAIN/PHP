248b56e75e31c14393079ba17572c03489511085
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-01-26 21:28:06 +0600

    routin-in-mysql-procedures

--- class26/iqus-routin-iprocedure/in-dex.php
+++ class26/iqus-routin-iprocedure/in-dex.php
@@ -1,10 +1,14 @@
 <?php
 require "database.php";
-$id = $_POST['id']??"by default testing id";
-$name = $_POST['name']??"by default testing name";
-$password = $_POST['password']??"by default testing password";
+$pid = $_POST['id']??"by default testing id";
+$pname = $_POST['name']??"by default testing name";
+$ppassword = $_POST['password']??"by default testing password";
+$create_at = $_POST['create_at']??"by default testing password";
+$connect->autocommit(false);//on thakle = in-dex.php= k refresh dile auto commit kore data by default ta niye ney.
 
-$query = "call public_info ('{$id},{$name},{$password}')";
+$query = "call public_info ('{$pid}','{$pname}','{$ppassword}')";
 $connect->query($query);
+//  echo $connect->insert_id . "<br>";
+//  echo $connect->affected_rows;
 
 ?>
