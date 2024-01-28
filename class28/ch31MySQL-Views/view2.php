3e9c68b0deea03bc84c1099f7c7e588079af875e
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-01-28 20:23:37 +0600

    Relationship in mysql

--- class28/ch31MySQL-Views/view2.php
+++ class28/ch31MySQL-Views/view2.php
@@ -1,10 +1,10 @@
 <?php
 $conn = new mysqli("localhost","root","","r57_php") or die("Error!!!");
-$view = "create or replace view productlt100 as select * from products where price < 100";
+$view = "create or replace view productGT400 as select * from products where price >= 400";
 $conn->query($view);
 echo "View is Created <br>";
 $sort = $_GET['sort'] ?? "id";
-$selectQ = "select * from productlt100 order by {$sort}";
+$selectQ = "select * from productGT400 order by {$sort}";
 $records = $conn->query($selectQ);
 if($records->num_rows){
     while($row = $records->fetch_assoc()){
