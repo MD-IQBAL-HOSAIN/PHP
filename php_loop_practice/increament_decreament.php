71b77d2965a9c4cf073e3b3db19a74dc21d05ea2
MD IQBAL HOSSAIN <152992612+MD-IQBAL-HOSAIN@users.noreply.github.com>
2024-03-09 20:34:07 +0600

    php loop practice

---
+++ php_loop_practice/increament_decreament.php
@@ -0,0 +1,17 @@
+<?php
+
+$amount = 10;
+$amount += 10;
+echo $amount."<br>";
+//its short form
+$a = 3;
+$a++;
+echo $a."<br>";
+
+echo "<br>";
+//decrement
+$a = 3;
+$a--;
+echo $a."<br>";
+
+?>
---
+++ php_loop_practice/loop_practice.php
@@ -0,0 +1,11 @@
+<?php
+echo "<h1>while loop practice.</h1>";
+$count = 1;
+while ($count <= 10) {
+    // echo "Bijoy chowdhury <br>";  
+    echo "$count: Bijoy chowdhury <br>";
+    $count++;
+    //2 kore baranor jonno
+    //$count+=2;
+}
+?>
